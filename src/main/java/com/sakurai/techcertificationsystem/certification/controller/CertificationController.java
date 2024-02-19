cfc269108f6e102c479169b11a97a16165edf432
Ryan Sakurai <ryansakurai@estudante.ufscar.br>
2024-02-19 02:07:17 -0300

    add resource URI to submitAnswers() response

--- src/main/java/com/sakurai/techcertificationsystem/certification/controller/CertificationController.java
+++ src/main/java/com/sakurai/techcertificationsystem/certification/controller/CertificationController.java
@@ -1,6 +1,8 @@
 package com.sakurai.techcertificationsystem.certification.controller;
 
+import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.sakurai.techcertificationsystem.certification.dto.SubmitionDto;
 import com.sakurai.techcertificationsystem.certification.model.Certification;
@@ -21,17 +24,27 @@ public class CertificationController {
     
     @Autowired
     private CertificationService certificationService;
+    
+    @PostMapping()
+    public ResponseEntity<Certification> submitAnswers(@RequestBody SubmitionDto submition, UriComponentsBuilder ucb) {
+        Certification certification = this.certificationService.submitAnswers(submition);
+        URI certificationUri = ucb
+        .path("/certifications/{id}")
+        .buildAndExpand(certification.getId())
+        .toUri();
+        return ResponseEntity.created(certificationUri).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Certification> getCertification(@PathVariable UUID id) {
+        // TO BE IMPLEMENTED
+        return ResponseEntity.ok().build();
+    }
 
     @GetMapping("/rankings/{quantity}")
     public ResponseEntity<List<Certification>> getRanking(@PathVariable int quantity) {
         List<Certification> ranking = certificationService.getRanking(quantity);
         return ResponseEntity.ok().body(ranking);
-    }
-
-    @PostMapping()
-    public ResponseEntity<Certification> submitAnswers(@RequestBody SubmitionDto submition) {
-        Certification certification = this.certificationService.submitAnswers(submition);
-        return ResponseEntity.created(null).body(certification);
     }
 
 }
